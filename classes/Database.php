e863be82dd6e92369e444ea091c6235dd8233909
Tomonori Ueno <t.uepon@gmail.com>
2019-03-07 06:01:11 +0900

    Fetching data OK.

--- classes/Database.php
+++ classes/Database.php
@@ -20,8 +20,41 @@ class Database{
     // Create new PDO
     try{
       $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-    } catch(PDOException $e){
+    } catch(PDOException $Exception){
+      $e = $message;
       $this->error = $e->getMessage();
     }
+  }
+
+  public function query($query){
+    $this->stmt = $this->dbh->prepare($query);
+  }
+
+  public function bind($param, $value, $type = null){
+    if(is_null($type)){
+      switch(true){
+        case is_int($value):
+          $type = PDO::PARAM_INT;
+          break;
+        case is_bool($value):
+          $type = PDO::PARAM_BOOL;
+          break;
+        case is_null($value):
+          $type = PDO::PARAM_NULL;
+          break;
+          default:
+          $type = PDO::PARAM_STR;
+      }
+    }
+    $this->stmt->bindValue($param, $value, $type);
+  }
+
+  public function execute(){
+    return $this->stmt->execute();
+  }
+
+  public function resultSet(){
+    $this->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 }
--- index.php
+++ index.php
@@ -2,3 +2,17 @@
 require 'classes/Database.php';
 
 $database = new Database;
+
+// Make a query
+$database->query('SELECT * FROM posts');
+$rows = $database->resultSet();
+?>
+<h1>Posts</h1>
+<div>
+<?php foreach($rows as $row) : ?>
+  <div>
+    <h3><?php echo $row['title']; ?></h3>
+    <p><?php echo $row['body']; ?></p>
+  </div>
+<?php endforeach; ?>
+</div>
